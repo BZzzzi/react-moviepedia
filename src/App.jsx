ebd1182ce018c0b119c08255cb26be24e579048a
BZzzzi <khj23465@naver.com>
2025-10-22 00:43:42 +0900

    Feat: 내림차순 정렬 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import ReviewList from "./components/ReviewList";
 import items from "./mock.json";
 
 function App() {
+  // 1. 사용자가 원하는 정렬을 클릭한다.
+  // 2. 클릭할 때 마다 State를 변경시켜 정렬할 때 리렌더링 된다.
+  const [order, setOrder] = useState();
+
+  // 3. 최신순, 베스트순이 내림차순으로 정렬된다.
+  const sortedItem = items.sort((a, b) => b[order] - a[order]);
+
   return (
     <>
-      <ReviewList items={items} />
+      <button onClick={() => setOrder("createdAt")}>최신순</button>
+      <button onClick={() => setOrder("rating")}>베스트순</button>
+      <ReviewList items={sortedItem} />
     </>
   );
 }
